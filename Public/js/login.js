483d12de3427d8b945b9624811a7ef62d2603749
Karelyn Rojas <knrojas@ucdavis.edu>
2023-08-12 17:31:42 -0700

    sign up . js seperated from login

--- Public/js/login.js
+++ Public/js/login.js
@@ -21,39 +21,6 @@ $(function() {
           alert(response.statusText);
         }
       }
-    };
-
-
-  
-    const signupFormHandler = async (event) => {
-      event.preventDefault();
-  
-      const username = document.querySelector('#username-signup').value.trim();         
-      const password = document.querySelector('#password-signup').value.trim();
-  
-      if (username && password) {                                  
-        const response = await fetch('/api/users', {
-          method: 'POST',
-          body: JSON.stringify({ username, password }),
-          headers: { 'Content-Type': 'application/json' },
-        });
-  
-        if (response.ok) {
-          document.location.replace('/dashboard');
-        } else {
-          alert(response.statusText);
-        }
-      }
-    };
-  
-    const showSignupForm = async(e) =>{
-      e.preventDefault();
-  
-      $(".signupButton").hide();
-      $(".signupPage").show();
-    }
-  
+    };  
     $( ".login-form" ).on( "submit", loginFormHandler)
-    $( ".signup-form" ).on( "submit", signupFormHandler)
-    $(".signupButton").click(showSignupForm)
   });
