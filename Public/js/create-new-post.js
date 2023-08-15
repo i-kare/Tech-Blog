2d428105951ce86e633d7aa0e62f899d51e09042
Karelyn Rojas <knrojas@ucdavis.edu>
2023-08-14 20:51:29 -0700

    handlebar displays adjusted

--- Public/js/create-new-post.js
+++ Public/js/create-new-post.js
@@ -1,26 +1,33 @@
 $(function() {
-    const postId = document.querySelector('input[name="post-Id"]').value;
-    console.log(postId)
+  const postId = document.querySelector('input[name="post-Id"]').value;
+  console.log(postId)
 
 
-    const commentingFormHandler = async (event) => {
-        event.preventDefault();
+  const commentingFormHandler = async (event) => {
+    event.preventDefault();
     
-        const commentingInformation= document.querySelector('textarea[name="commenting-body"]').value;         
+    const commentingInformation= document.querySelector('textarea[name="commenting-body"]').value;         
 
-        if (commentingInformation) {                                  
-          const response = await fetch('/api/comment', {
-            method: 'POST',
-            body: JSON.stringify({ commentingInformation, postId }),
-            headers: { 'Content-Type': 'application/json' },
-          });
+    if (commentingInformation) {                                  
+      const response = await fetch('/api/comment', {
+        method: 'POST',
+        body: JSON.stringify({ commentingInformation, postId }),
+        headers: { 'Content-Type': 'application/json' },
+      });
     
-          if (response.ok) {
-            document.location.reload();
-          } else {
-            alert(response.statusText);
-          }
-        }
-      };
-      $("#commenting-form").on("submit", commentingFormHandler)
-    });
+      if (response.ok) {
+        document.location.reload();
+      } else {
+        alert(response.statusText);
+      }
+    }
+  };
+  const showcreateCommentForm = async(e) =>{
+    e.preventDefault();
+
+    $("body").hide();
+    $("dashboardPage").show();
+  }
+  $("btn btn-primary btn-lg").click(showcreateCommentForm)
+  $("#commenting-form").on("submit", commentingFormHandler)
+});
